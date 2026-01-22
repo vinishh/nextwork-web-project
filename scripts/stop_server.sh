50c2d46b8b65518a6310d5a4ecd7c46d1db04430
vinishh <136488215+vinishh@users.noreply.github.com>
2026-01-22 13:14:09 -0500

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# Stop Apache if it is running
 isExistApp="$(pgrep httpd)"
 if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
+    sudo systemctl stop httpd.service
 fi
+
+# Stop Tomcat if it is running
 isExistApp="$(pgrep tomcat)"
 if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
+    sudo systemctl stop tomcat.service
 fi
+
+# Clean up the existing webapp directory and war to prevent conflicts
+sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project
+sudo rm -f /usr/share/tomcat/webapps/nextwork-web-project.war
